6cac8489b6e133a56821a2328d3d4bcab38b3bb8
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-05-26 17:59:10 +0200

    main-js upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
 const API_KEY = '44081515-c4a529d06247cd72f265dbdd7';
 const BASE_URL = 'https://pixabay.com/api/';
+export const PER_PAGE = 15;
 
-export const fetchPhotos = query => {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
+import axios from 'axios';
+
+axios.defaults.baseURL = BASE_URL;
+
+export const fetchPhotos = async (query, photosCurrentPage = 1) => {
+  const response = await axios.get('', {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: PER_PAGE,
+      page: photosCurrentPage,
+    },
   });
 
-  return fetch(`${BASE_URL}/?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    }
-    return response.json();
-  });
+  return response.data;
 };
